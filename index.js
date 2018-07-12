221bdef8d1b47b7e26528c653ed424c9453205be
faithalynelong <falong313@pccsk12.com>
2018-07-12 17:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(names){
 for (let i = 0; i < names.length; i++) {
-  console.log('Welcome ${names}! You are empolyee #${i + 1}.');
+  console.log('Welcome ${names}! You are employee #${i + 1}.');
 }
 return names;
 }
