ec7740c386d388a625c8101e121247d7f5ac6528
faithalynelong <falong313@pccsk12.com>
2018-07-12 18:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// Code your solutions in this file
-function printBadges(names){
+function printBadges (names) {
   for (let i = 0; i < names.length; i++) {
-    console.log("Welcome " + names[i] + "! You are employee #" + (i+ 1) + ".");
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
+
   return names;
 }
 
-
-function tailsNeverFails(coins){
+function tailsNeverFails () {
   let tails = 0;
+
   while (Math.random() >= 0.5) {
     tails++;
   }
-  return 'You got ${tails} tails in a row!';
+
+  return `You got ${tails} tails in a row!`;
 }
